fc24a3cdfa3c4abbfcd0f07531c84f9e69f38046
CarolTeodoro <caroline.r.teodoro@gmail.com>
2022-06-20 19:27:50 -0300

    primeiro-commit

---
+++ src/desafio1.js
@@ -0,0 +1,8 @@
+"use strict";
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+;
+var employee1 = {
+    name: 'Eduardo',
+    code: 555,
+    role: 'manager'
+};
---
+++ src/desafio1.ts
@@ -0,0 +1,13 @@
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+interface employee {
+    name : String,
+    code : Number,
+    role : String
+};
+
+let employee1:employee = {
+    name:'Eduardo',
+    code: 555,
+    role:'manager'
+};
